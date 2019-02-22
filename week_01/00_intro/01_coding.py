7e5db7b0bf2f4658a557dc7afcf93025a9937293
Daniel Wegmann <danielwegmann@Daniels-MacBook-Pro.local>
2019-02-22 16:14:18 +0800

    exercises week1 intro

--- week_01/00_intro/01_coding.py
+++ week_01/00_intro/01_coding.py
@@ -6,3 +6,13 @@ instead of writing it 3 times.
 Time for co-co-co-ding.
 
 '''
+
+#print( "Time for " + "co-"*3 + "ding")
+
+#print("coding"[-4:])
+
+
+x = ""
+for i in range(3):
+    x = x+ "co-"
+    print(i,x)
