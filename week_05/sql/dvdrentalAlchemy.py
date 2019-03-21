d2c67d5762df9c7abd3572b67e4f02e1fdb05fcf
Daniel Wegmann <danielwegmann@gmail.com>
2019-03-21 12:07:15 +0800

    week5 SQL

---
+++ week_05/sql/dvdrentalAlchemy.py
@@ -0,0 +1,10 @@
+import sqlalchemy as sqa
+from secrets import username, password
+from pprint import pprint
+
+
+DATABASE_URI = f'postgres+psycopg2://{username}:{password}@localhost:5432/dvdrentals'
+
+engine = sqa.create_engine(DATABASE_URI)
+connection = engine.connect()
+metadata = sqa.MetaData()
